13acb988076ea9861d491ab26de5f0568ab0b63b
komilovsardorbek <sardork435@gmail.com>
2022-02-20 20:12:07 +0500

    root

---
+++ assets/js/boottable.js
@@ -0,0 +1,170 @@
+var i = 1;
+function childrenRow() {
+    i++;
+    $('#childTable').find('tbody').append('<tr><th scope="row">' + i +
+        '</th><td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td \n' + 'class="fa fa-plus-circle f-20 txt-primary"\n' +
+        'id="addrow" onclick="childrenRow()"\n' + '/></td>\n' +'</td></tr>');
+}
+
+var j = 1;
+function childrenTable() {
+    j++;
+    $('#childTable1').find('tbody').append('<tr><th scope="row">' + j + '</th>' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' +'placeholder=""></td>\n' +
+        '<td \n' +'class="fa fa-plus-circle f-20 txt-primary"\n' +
+        'id="addrow1" onclick="childrenTable()"\n' +'/></td>\n' +'</td></tr>');
+}
+
+var a = 1;
+function childrentable() {
+    a++;
+    $('#childTable2').find('tbody').append('<tr><th class="table-primary" scope="row">' + a +
+        '</th><td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td \n' + 'class="fa fa-plus-circle f-20 txt-primary"\n' +
+        'id="addrow1" onclick="childrentable()"\n' + '/></td>\n' +'</td></tr>');
+}
+
+var x = 1;
+function childrentabl1() {
+    x++;
+    $('#childTable3').find('tbody').append('<tr><th scope="row">' + x +
+        '</th>' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td \n' + 'class="fa fa-plus-circle f-20 txt-primary"\n' +
+        'id="addrow3" onclick="childrentabl1()"\n' + '/></td>\n' +'</td></tr>');
+}
+var y = 1;
+function childrentabl2() {
+    y++;
+    $('#childTable4').find('tbody').append('<tr><th scope="row">' + y +
+        '</th>' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td \n' + 'class="fa fa-plus-circle f-20 txt-primary"\n' +
+        'id="addrow4" onclick="childrentabl2()"\n' + '/></td>\n' +'</td></tr>');
+}
+var z = 1;
+function childrentabl3() {
+    z++;
+    $('#childTable5').find('tbody').append('<tr><th scope="row">' + z +
+        '</th>' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td \n' + 'class="fa fa-plus-circle f-20 txt-primary"\n' +
+        'id="addrow5" onclick="childrentabl3()"\n' + '/></td>\n' +'</td></tr>');
+}
+var q = 1;
+function childrentabl4() {
+    q++;
+    $('#childTable6').find('tbody').append('<tr><th scope="row">' + q +
+        '</th>' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td \n' + 'class="fa fa-plus-circle f-20 txt-primary"\n' +
+        'id="addrow6" onclick="childrentabl4()"\n' + '/></td>\n' +'</td></tr>');
+}
+var r = 1;
+function childrentabl5() {
+    r++;
+    $('#childTable7').find('tbody').append('<tr><th scope="row">' + r +
+        '</th>' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td \n' + 'class="fa fa-plus-circle f-20 txt-primary"\n' +
+        'id="addrow7" onclick="childrentabl5()"\n' + '/></td>\n' +'</td></tr>');
+}
+var t = 1;
+function childrentabl6() {
+    t++;
+    $('#childTable8').find('tbody').append('<tr><th scope="row">' + t +
+        '</th>' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td \n' + 'class="fa fa-plus-circle f-20 txt-primary"\n' +
+        'id="addrow8" onclick="childrentabl6()"\n' + '/></td>\n' +'</td></tr>');
+}
+var h = 1;
+function childrentabl7() {
+    h++;
+    $('#childTable9').find('tbody').append('<tr><th scope="row">' + h +
+        '</th>' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td \n' + 'class="fa fa-plus-circle f-20 txt-primary"\n' +
+        'id="addrow9" onclick="childrentabl7()"\n' + '/></td>\n' +'</td></tr>');
+}
+var l = 1;
+function childrentablseven() {
+    l++;
+    $('#childTableTen').find('tbody').append('<tr><th scope="row">' + l +
+        '</th>' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td \n' + 'class="fa fa-plus-circle f-20 txt-primary"\n' +
+        'id="addrowten" onclick="childrentablseven()"\n' + '/></td>\n' +'</td></tr>');
+}
+var l = 1;
+function childrentableight() {
+    l++;
+    $('#childTableEleven').find('tbody').append('<tr><th scope="row">' + l +
+        '</th>' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td><input class="border-0 w-100" type="text"\n' + 'placeholder=""></td>\n' +
+        '<td \n' + 'class="fa fa-plus-circle f-20 txt-primary"\n' +
+        'id="addroweleven" onclick="childrentableight()"\n' + '/></td>\n' +'</td></tr>');
+}
